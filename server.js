e3de5d7d709431c76982bc8c45da057697f2913d
tanmayVaish <tanmay777leon@gmail.com>
2022-05-04 20:05:17 +0000

    https config

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const http = require("http");
+const https = require("https");
+
+const fs = require("fs");
+
+console.log({env:process.env});
+console.log("privateKey",process.env.SSL_PRIVATE_KEY);
+console.log("cert",process.env.SSL_CERTIFICATE);
+
+const private_key = fs.readFileSync(process.env.SSL_PRIVATE_KEY, "utf8");
+const certificate = fs.readFileSync(process.env.SSL_CERTIFICATE, "utf8");
+
+
+const credentials = { key: private_key, cert: certificate };
 
 const app = express();
 
@@ -10,4 +26,12 @@ app.use(
 		changeOrigin: true,
 	})
 );
-app.listen(3000);
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+const PORT = process.env.PORT || 80;
+const HTTPS_PORT = process.env.HTTPS_PORT || 443;
+
+httpServer.listen(PORT, ()=> console.log("hosting http on", PORT));
+httpsServer.listen(HTTPS_PORT, ()=> console.log("hosting https on", HTTPS_PORT));
