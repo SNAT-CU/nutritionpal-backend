613b5a47d2969d931fc24f64fdc7561a0c16db47
tanmayVaish <tanmay777leon@gmail.com>
2022-05-09 21:47:27 +0530

    restructured server.js

--- server.js
+++ server.js
@@ -1,39 +1,13 @@
-require("dotenv").config();
 const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-var cors = require("cors");
-
-app.use(cors());
-
-const http = require("http");
-// const https = require("https");
-//
-// const fs = require("fs");
-
-// console.log({env:process.env});
-// console.log("privateKey",process.env.SSL_PRIVATE_KEY);
-// console.log("cert",process.env.SSL_CERTIFICATE);
-
-// const private_key = fs.readFileSync(process.env.SSL_PRIVATE_KEY, "utf8");
-// const certificate = fs.readFileSync(process.env.SSL_CERTIFICATE, "utf8");
-
-// const credentials = { key: private_key, cert: certificate };
-
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(
-	"/",
-	createProxyMiddleware({
-		target: "https://platform.fatsecret.com/",
-		changeOrigin: true,
-	})
-);
+app.get("/", async(req, res) => {
+	res.json({
+		message: "Hello World!"
+	});
+});
 
-const httpServer = http.createServer(app);
-// const httpsServer = https.createServer(credentials, app);
-
-const PORT = process.env.PORT || 80;
-// const HTTPS_PORT = process.env.HTTPS_PORT || 443;
-
-httpServer.listen(PORT, () => console.log("hosting http on", PORT));
-// httpsServer.listen(HTTPS_PORT, ()=> console.log("hosting https on", HTTPS_PORT));
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
